8f237bd5f07167c90cc3cd9f2ca737c3b8ec4ed0
Keith Fike <fkeithr@gmail.com>
2020-06-20 15:57:06 -0600

    added dom elements

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-$('.card').mouseover(function() {
+var cardWrapperEl = document.querySelector("#scrolling-wrapper");
+var cardEl = document.querySelector("#card");
+var suggestionsEl = document.querySelector("#suggestions");
 
+$('.card').mouseover(function() {
+  var infoEl = document.createElement("p");
+  infoEl.classList("");
+  infoEl.innerText = "lorem ipsum ;dlkfha;slkdjfa;lskjdf;lsjdf;lkj;laskjg;lfkjg;ldkjgl;kasjaslkdfj";
+
+  infoEl.appendChild(suggestionsEl);
 });
 
 $('.card').mouseout(function() {
