1454eec73539b7c2692e8412d62d2cbba743e8f5
Keith Fike <fkeithr@gmail.com>
2020-06-21 16:57:43 -0600

    added card generator and event listeners for eah card

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,15 +1,31 @@
 var cardWrapperEl = document.querySelector("#scrolling-wrapper");
-var cardEl = document.querySelector("#card");
 var suggestionsEl = document.querySelector("#suggestions");
+var gameInfoEl = document.querySelector("#game-info");
+var infoEl = document.createElement("p");
+var gameCards = [];
 
-$('.card').mouseover(function() {
-  var infoEl = document.createElement("p");
-  infoEl.classList("");
+var cardGenerator = function(numberOfGames) {
+  for (i = 0; i < numberOfGames; i++) {
+    gameCards[i] = document.createElement("div");
+    gameCards[i].innerHTML = "<h3></h3>";
+    gameCards[i].classList = "card";
+    gameCards[i].id = "card" + i;
+    cardWrapperEl.appendChild(gameCards[i]);
+  }
+  console.log(gameCards);
+
+}
+
+cardGenerator(5);
+
+gameCards.forEach(element => element.addEventListener("mouseover", function() {
+  console.log("boiiiiii");
+  infoEl.classList = "text-center not-permanant";
   infoEl.innerText = "lorem ipsum ;dlkfha;slkdjfa;lskjdf;lsjdf;lkj;laskjg;lfkjg;ldkjgl;kasjaslkdfj";
 
-  infoEl.appendChild(suggestionsEl);
-});
+  gameInfoEl.appendChild(infoEl);
+})); 
 
-$('.card').mouseout(function() {
-
-});
+//gameCards.addEventListener("mouseout", function(event) {
+//  infoEl.parentNode.removeChild(infoEl);
+//});
