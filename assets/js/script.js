85d6ec1eef57a3a135c51bd7251175132900c4be
Jarred Baca <jarredbaca@MacBook-Pro.local>
2020-06-21 00:57:02 -0600

    added CSS to Quiz page and finished basic quiz function JB

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
+//global variables for the questions and answers
 var questions = ["What platform do you currently play on?","What game genre do you prefer?", ""];
 var platformOptions = ["Xbox","Playstation","PC","Nintendo"]
 var genreOptions = ["Action", "RPG", "Sports","Shooter",]
 
+// variables created to make jquery easier to manipulate
 var btn1 = $("#A");
 var btn2 = $("#B");
 var btn3 = $("#C");
@@ -30,6 +32,7 @@ $("#start-button").on("click", function() {
     generateQuestion0();
 })
 
+
 var generateQuestion0 = function() {
     header.append(questions[0]);
     answerSection.removeClass("hidden");
@@ -38,6 +41,31 @@ var generateQuestion0 = function() {
     btn2.append(platformOptions[1]);
     btn3.append(platformOptions[2]);
     btn4.append(platformOptions[3]);
+
+    btn1.on("click", function() {
+        generateQuestion1();
+    });
+
+    btn2.on("click", function() {
+        generateQuestion1();
+    });
+
+    btn3.on("click", function() {
+        generateQuestion1();
+    });
+
+    btn4.on("click", function() {
+        generateQuestion1();
+    });
+
+}
+
+var generateQuestion1 = function () {
+    header.html('');
+    btn1.html('');
+    btn2.html('');
+    btn3.html('');
+    btn4.html('');
     
     header.append(questions[1]);
 
@@ -45,8 +73,58 @@ var generateQuestion0 = function() {
     btn2.append(genreOptions[1]);
     btn3.append(genreOptions[2]);
     btn4.append(genreOptions[3]);
+
+    btn1.on("click", function() {
+        generateQuestion2();
+    });
+
+    btn2.on("click", function() {
+        generateQuestion2();
+    });
+
+    btn3.on("click", function() {
+        generateQuestion2();
+    });
+
+    btn4.on("click", function() {
+        generateQuestion2();
+    });
 }
 
+var generateQuestion2 = function () {
+    header.html('');
+    btn1.html('');
+    btn2.html('');
+    btn3.html('');
+    btn4.html('');
+
+    header.append(questions[1]);
+
+    btn1.append(genreOptions[0]);
+    btn2.append(genreOptions[1]);
+    btn3.append(genreOptions[2]);
+    btn4.append(genreOptions[3]);
+
+    btn1.on("click", function() {
+        
+    });
+
+    btn2.on("click", function() {
+        
+    });
+
+    btn3.on("click", function() {
+        
+    });
+
+    btn4.on("click", function() {
+        
+    });
+}
+
+var reDirect = function () {
+
+}
 
 
 
