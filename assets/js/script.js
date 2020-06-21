004278fe44298e11f490af96083b6af5e3b2bd52
Jarred Baca <jarredbaca@MacBook-Pro.local>
2020-06-20 22:24:37 -0600

    reorganized javascript JB

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
-var questions = ["What platform do you currently play on?","What game genre do you prefer?"];
+var questions = ["What platform do you currently play on?","What game genre do you prefer?", ""];
+var platformOptions = ["Xbox","Playstation","PC","Nintendo"]
+var genreOptions = ["Action", "RPG", "Sports","Shooter",]
 
-console.log(questions);
+var btn1 = $("#A");
+var btn2 = $("#B");
+var btn3 = $("#C");
+var btn4 = $("#D");
+var header = $("#question-header");
+var answerSection = $("#answer-section")
  
 var gameData = function () {
-    var apiURL = "https://api.rawg.io/api/";
+    var apiURL = "https://api.rawg.io/api/platforms";
     $.ajax({
         url: apiURL,
         method: "GET"
@@ -20,19 +27,28 @@ $("#start-button").on("click", function() {
     $("#start-button").remove();
     $("#questions").addClass("card");
 
-    generateQuestions();
+    generateQuestion0();
 })
 
-var generateQuestions = function() {
-    for ( var i =0; i < questions.length; i++){
-        if (i == 0) {
-            $("#quiz-content").append(questions[0]);
-        }
+var generateQuestion0 = function() {
+    header.append(questions[0]);
+    answerSection.removeClass("hidden");
 
-        if (i == 1) {
-            
-        }
-    }
+    btn1.append(platformOptions[0]);
+    btn2.append(platformOptions[1]);
+    btn3.append(platformOptions[2]);
+    btn4.append(platformOptions[3]);
+    
+    header.append(questions[1]);
+
+    btn1.append(genreOptions[0]);
+    btn2.append(genreOptions[1]);
+    btn3.append(genreOptions[2]);
+    btn4.append(genreOptions[3]);
 }
+
+
+
+
 
 gameData();
