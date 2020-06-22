74c413dddb5e0063b82c1e03692779126268a744
Jarred Baca <jarredbaca@MacBook-Pro.local>
2020-06-21 21:32:38 -0600

    finalized quiz function with local storage behaving correctly JB

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 //global variables for the questions and answers
-var questions = ["What platform do you currently play on?","What game genre do you prefer?", "Would you prefer games released within the year or anytime?"];
+var questions = ["What platform do you currently play on?","What game genre do you prefer?", "Would you prefer games released within the last month or anytime?"];
 var platformOptions = ["Xbox","Playstation","PC","Nintendo"]
-var genreOptions = ["Action", "RPG", "Sports","Shooter",]
-var releaseOptions = ["Anytime", "This Year"]
+var genreOptions = ["Action", "Indie", "Adventure","Shooter",]
 
 // variables created to make jquery easier to manipulate
 var btn1 = $("#A");
@@ -11,10 +10,11 @@ var btn3 = $("#C");
 var btn4 = $("#D");
 var header = $("#question-header");
 var answerSection = $("#answer-section")
+var dataArray = [];
 
- 
+//used to view datapoints within API 
 var gameData = function () {
-    var apiURL = "https://api.rawg.io/api/games";
+    var apiURL = "https://api.rawg.io/api/platforms";
     $.ajax({
         url: apiURL,
         method: "GET"
@@ -24,9 +24,9 @@ var gameData = function () {
 
     })
 }
-
+//Same as above
 var platformData = function () {
-    var apiURL = "https://api.rawg.io/api/platforms";
+    var apiURL = "https://api.rawg.io/api/genres";
     $.ajax({
         url: apiURL,
         method: "GET"
@@ -57,101 +57,88 @@ var generateQuestion0 = function() {
     btn4.append(platformOptions[3]);
 
     btn1.on("click", function() {
-        var Xbox = "Xbox"
-        console.log(answerArray);
-        localStorage.setItem("Xbox", Xbox);
+        var Xbox = 3;
+        localStorage.setItem("Answer1", JSON.stringify(Xbox));
         generateQuestion1();
     });
 
     btn2.on("click", function() {
-        var Playstation = "Playstation"
-        localStorage.setItem("Playstation",Playstation);
+        var Playstation = 2;
+        localStorage.setItem("Answer1",JSON.stringify(Playstation));
         generateQuestion1();
     });
 
     btn3.on("click", function() {
-        var PC = "PC"
-        localStorage.setItem("PC",PC);
+        var PC = 1;
+        localStorage.setItem("Answer1",JSON.stringify(PC));
         generateQuestion1();
     });
 
     btn4.on("click", function() {
-        var Nintendo = "Nintendo"
-        localStorage.setItem("Nintendo",Nintendo);
+        var Nintendo = 7;
+        localStorage.setItem("Answer1",JSON.stringify(Nintendo));
         generateQuestion1();
     });
 
 }
 
 var generateQuestion1 = function () {
-    header.html('');
-    btn1.html('');
-    btn2.html('');
-    btn3.html('');
-    btn4.html('');
-    
+    resetButtons();
+    createNewButtons();
+
     header.append(questions[1]);
 
-    btn1.append(genreOptions[0]);
-    btn2.append(genreOptions[1]);
-    btn3.append(genreOptions[2]);
-    btn4.append(genreOptions[3]);
+    var btn5 = $("#E");
+    var btn6 = $("#F");
+    var btn7 = $("#G");
+    var btn8 = $("#H");
 
-    btn1.on("click", function() {
-        var Action = "Action";
-        localStorage.setItem("Action",Action);
-        console.log(localStorage);
-        generateQuestion2();
+    btn5.append(genreOptions[0]);
+    btn6.append(genreOptions[1]);
+    btn7.append(genreOptions[2]);
+    btn8.append(genreOptions[3]);
+
+    btn5.on("click", function() {
+        var action = "action";
+        localStorage.setItem("Answer2",JSON.stringify(action));
     });
 
-    btn2.on("click", function() {
-        var RPG = "RPG";
-        localStorage.setItem("RPG", RPG);
-        generateQuestion2();
+    btn6.on("click", function() {
+        var indie = "indie";
+        localStorage.setItem("Answer2",JSON.stringify(indie))
     });
 
-    btn3.on("click", function() {
-        var Sports = "Sports";
-        localStorage.setItem("Sports",Sports);
-        generateQuestion2();
+    btn7.on("click", function() {
+        var adventure = "adventure";
+        localStorage.setItem("Answer2",JSON.stringify(adventure))
     });
 
-    btn4.on("click", function() {
-        var Shooter = "Shooter";
-        localStorage.setItem("Shooter",Shooter);
-        generateQuestion2();
+    btn8.on("click", function() {
+        var shooter = "shooter";
+        localStorage.setItem("Answer2",JSON.stringify(shooter))
     });
 }
 
-var generateQuestion2 = function () {
+
+var resetButtons = function () {
     header.html('');
-    btn1.html('');
+    btn1.html ('');
     btn2.html('');
     btn3.html('');
     btn4.html('');
-
-    header.append(questions[2]);
-
-    btn1.append(releaseOptions[0]);
-    btn2.append(releaseOptions[1]);
-
-    btn3.remove();
-    btn4.remove();
-
-    btn1.on("click", function() {
-        
-    });
-
-    btn2.on("click", function() {
-        
-    });
-
 }
 
-var reDirect = function () {
-
+var createNewButtons = function () {
+    btn1.replaceWith('<button type ="button" id = "E" class="answer-button"> </button>');
+    btn2.replaceWith('<button type ="button" id = "F" class="answer-button"> </button>');
+    btn3.replaceWith('<button type ="button" id = "G" class="answer-button"> </button>');
+    btn4.replaceWith('<button type ="button" id = "H" class="answer-button"> </button>');
 }
 
 
 gameData();
 platformData();
+
+var reDirect = function () {
+
+}
